5365c84f24c6bc0c5376ab8682b0f8bf7579f82d
Ulises López <ulises050794@gmail.com>
2021-02-08 18:28:16 -0600

    .env added

--- src/datasources/mongo-todo-atlas.datasource.ts
+++ src/datasources/mongo-todo-atlas.datasource.ts
@@ -1,16 +1,19 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 const config = {
-  name: 'MongoTodoAtlas',
-  connector: 'mongodb',
-  url: 'mongodb+srv://todo:todo@cluster0.i93l0.mongodb.net/todo',
+  name: process.env.NAME,
+  connector: process.env.CONNECTOR,
+  url: process.env.URL,
   host: '',
   port: 0,
-  user: 'todo',
-  password: 'todo',
-  database: 'todo',
-  useNewUrlParser: true
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  useNewUrlParser: process.env.USE_NEW_URL_PARSER
 };
 
 // Observe application's life cycle to disconnect the datasource when
@@ -22,7 +25,6 @@ export class MongoTodoAtlasDataSource extends juggler.DataSource
   implements LifeCycleObserver {
   static dataSourceName = 'MongoTodoAtlas';
   static readonly defaultConfig = config;
-
   constructor(
     @inject('datasources.config.MongoTodoAtlas', {optional: true})
     dsConfig: object = config,

beb957acb4a008126eba73c0574fb7c487a4bc0a
Ulises López <ulises050794@gmail.com>
2021-02-10 20:04:27 -0600

    comments deleted

--- src/datasources/mongo-todo-atlas.datasource.ts
+++ src/datasources/mongo-todo-atlas.datasource.ts
@@ -16,10 +16,7 @@ const config = {
   useNewUrlParser: process.env.USE_NEW_URL_PARSER
 };
 
-// Observe application's life cycle to disconnect the datasource when
-// application is stopped. This allows the application to be shut down
-// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
-// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+
 @lifeCycleObserver('datasource')
 export class MongoTodoAtlasDataSource extends juggler.DataSource
   implements LifeCycleObserver {

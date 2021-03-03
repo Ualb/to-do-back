a9044c8d8319748c49a4aedd73e5c5c409ff96e7
Ulises López <ulises050794@gmail.com>
2021-03-02 18:12:35 -0600

    nuevo modelo para tasks

---
+++ src/models/task.model.ts
@@ -0,0 +1,57 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Task extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  listID: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  isComplete: boolean;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  hourFinish: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  remember: boolean;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Task>) {
+    super(data);
+  }
+}
+
+export interface TaskRelations {
+  // describe navigational properties here
+}
+
+export type TaskWithRelations = Task & TaskRelations;
